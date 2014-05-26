e286b7b90eecfba1324270cc96d24be6547f05c2
Josh Lieberman <josh.lieberman92@gmail.com>
2014-05-26 19:12:56 -0400

    working on error handling

--- jessie.py
+++ jessie.py
@@ -3,8 +3,8 @@ import pyimgur
 import string
 import random
 import json
-
-from urllib2 import Request, urlopen
+import urllib2
+import requests
 
 # http://stackoverflow.com/questions/2257441/python-random-string-generation-with-upper-case-letters-and-digits
 def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
@@ -21,20 +21,32 @@ CLIENT_ID = "Your_applications_client_id"
 PATH = sys.argv[1]
 
 # TODO: error checking here
+
 im = pyimgur.Imgur(CLIENT_ID)
-uploaded_image = im.upload_image(PATH, title=PATH)
+
+try:
+	uploaded_image = im.upload_image(PATH, title=PATH)
+except ValueError, e:
+	print e
+	sys.exit()
+except requests.exceptions.HTTPError:
+	print "Error: check your CLIENT_ID"
+	sys.exit()
 # print(uploaded_image.title)
 # print(uploaded_image.link)
 # print(uploaded_image.size)
 # print(uploaded_image.type)
 
 headers = {"Content-Type": "application/json"}
-request = Request("http://upload.gfycat.com/transcode/" + id_generator() + "?fetchUrl=" + uploaded_image.link, headers=headers)
-response_body = urlopen(request).read()
-# print response_body
-# print type(response_body)
+try:
+	request = urllib2.Request("http://upload.gfycat.com/transcode/" + id_generator() + "?fetchUrl=" + uploaded_image.link, headers=headers)
+	response_body = urllib2.urlopen(request).read()
+	json_req = json.loads(response_body)
 
-json_req = json.loads(response_body)
-# TODO: Need error checking here
+	# print response_body
+	# print type(response_body)
+	# TODO: Need error checking here
 
-print "http://gfycat.com/" + json_req['gfyname']
+	print "http://gfycat.com/" + json_req['gfyname']
+except NameError:
+	print "Error: no response"
